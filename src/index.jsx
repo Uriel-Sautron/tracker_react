4e8fddfb50257ac4a680f18a044dfa836efeb321
Uriel <uriel.sautron@gmail.com>
2021-02-03 09:19:32 +0100

    First commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => (
+  <div className="container">
+    <h1>Bienvenue sur le traqueur de lecture</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
